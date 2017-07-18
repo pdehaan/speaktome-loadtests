08e5c2d790375e3c3caae687d94a4ccecec317db
Peter deHaan <peter@deseloper.com>
2017-07-18 15:34:53 -0700

    Tweak test and readme

--- README.md
+++ README.md
@@ -4,4 +4,20 @@ Load tests for https://github.com/mozilla/speaktome
 
 ## Sample data:
 
-To create some sample OPUS files, run `./record.sh` to record some 4 second clips and submit them to the proxy server.
+To create some sample OPUS files, run `./record.sh` to record some 4 second clips and submit them to the proxy server. You can find the generated `*.opus` files and the proxy server responses in the `tmp/` folder.
+
+## Requirements:
+
+Python 3+
+requests
+virtualenv
+
+## Setup:
+
+```sh
+$ virtualenv venv -p python3
+$ source ./venv/bin/activate
+$ pip install -r requirements.txt
+
+$ python test.py
+```
--- test.py
+++ test.py
@@ -5,15 +5,23 @@ url = 'https://speaktome.stage.mozaws.net/asr'
 
 
 def post_opus(filename):
-    files = {'file': open(filename, 'rb')}
-    headers = {}
-    headers['Content-Type'] = 'application/octet-stream'
+    try:
+        files = {'file': open(filename, 'rb')}
+        headers = {}
+        headers['Content-Type'] = 'application/octet-stream'
 
-    res = requests.post(url, files=files, headers=headers)
-    print(res.status_code)
-    return json.loads(res.text)
+        res = requests.post(url, files=files, headers=headers)
+        data = json.loads(res.text)
+        assert res.status_code < 400
+        assert data['status'] == 'ok'
+        return data
 
-res = post_opus('tmp/test2.opus')
+    except FileNotFoundError:
+        print('Unable to find file, {0}'.format(filename))
+    except:
+        print("huh?")
+
+res = post_opus('tmp/test5.opus')
 print(res['status'])
 
 # curl -H "Content-Type: application/octet-stream" --data-binary @tmp/test$i.opus https://speaktome.stage.mozaws.net/asr
